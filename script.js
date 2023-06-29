14777e9229def12ab9a97d3937618c1d6be3f642
Goldy2488 <goldy22@navgurukul.org>
2023-06-30 00:32:01 +0530

    inicial commit

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-document.getElementById("btn").addEventListener("click",function(){
-    let p1=Math.floor(Math.random()*6)+1;
-    let p2=Math.floor(Math.random()*6)+1;
-    document.getElementById("player1").src=`assets/${p1}.png`;
-    document.getElementById("player2").src=`assets/${p2}.png`;
+function makeSound() {
+    let audio = new Audio("assets/sound.mp3"); //for sound                                                  
+    audio.play();
+  }
+
+document.getElementById("start").addEventListener("click", function () {
+    makeSound();
+    let p1 = Math.floor(Math.random() * 6) + 1;
+    let p2 = Math.floor(Math.random() * 6) + 1;
+    document.getElementById("p1").src = `assets/${p1}.png`;
+    document.getElementById("p2").src = `assets/${p2}.png`;
     if (p1>p2){
-        document.querySelector("h1").textContent="Player 1 win"; }
-    else if (p1<p2) document.querySelector("h1").textContent="Player 2 win";
-    else document.querySelector("h1").textContent="game tie"
+        document.querySelector("h1").textContent="🥳 Player 1 win 👑"; 
+        let win = new Audio("assets/win.mp3").play();
+    }
+    else if (p1<p2) document.querySelector("h1").textContent="👑 Player 2 win 🥳";
+    else document.querySelector("h1").textContent="🤹 game tie 🤹‍♀️"
 }) 
+
+
